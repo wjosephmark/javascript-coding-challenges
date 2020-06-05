df10c395d552a4fbc35c3a9c7e8b3693e67a9b7d
wjosephmark <wjosephmark@gmail.com>
2020-06-05 11:41:59 -0600

    completed square number challenge

--- square-number.js
+++ square-number.js
@@ -1,3 +1,14 @@
 var isSquare = function(number){
-    
+
+    const sqrtNumber = Math.sqrt(number)
+
+    var result = false
+
+    if(Number.isInteger(sqrtNumber)){
+        result = true
+    }
+
+    return result
 }
+
+isSquare(25)
