aa034173ec629d2c8f3c552e81abbf9a105fe27d
wjosephmark <wjosephmark@gmail.com>
2020-08-19 20:02:31 -0600

    added reverse string funstion

--- maskify.js
+++ maskify.js
@@ -1,12 +1,25 @@
 function maskify(cc) {
-    
-    const ccLength = cc.length
 
-    console.log(ccLength)
+    function reverseString(str) {
+        var newString = "";
+        for (var i = str.length - 1; i >= 0; i--) {
+            newString += str[i];
+        }
+        return newString;
+    }
+
+    const backwards = reverseString(cc)
+
+    const lastFour = backwards.slice(0,4)
+
+    const hiddenLength = cc.length-4
+
+    console.log(lastFour)
+
+    return 
+
 }
 
 const cardNumber =  "adfasdfasdfasdfasdfasdfasdf"
-
-
 
 maskify(cardNumber)
