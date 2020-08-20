43821f324878cd8bc1dda4d40135b7a24c61eef5
wjosephmark <wjosephmark@gmail.com>
2020-08-19 21:05:59 -0600

    added not enough space challenge

--- maskify.js
+++ maskify.js
@@ -1,34 +1,53 @@
+// function maskify(cc) {
+
+//     if(4 < cc.length) {
+
+//         console.log("shitfuck")
+
+
+//         const numberString = cc.toString()
+        
+//         const numberLength = numberString.length
+        
+//         const hiddenLength = numberLength-4
+    
+//         const numberSlice = numberString.slice(hiddenLength, numberLength) 
+        
+//         const newNumber = []
+    
+//         const test = []
+    
+//         var i = 0
+//         while( i < numberLength-4){
+//             newNumber.push("#")
+//             i += 1
+//         }
+    
+//         newNumber.push(numberSlice)
+    
+    
+//         const numberJoined = newNumber.join('')
+    
+//         return(
+//             numberJoined
+//         )
+//     } else {
+//         return(
+//             cc
+//         )
+//     }
+
+
+// }
+
+const cardNumber =  1242
+
+
 function maskify(cc) {
-
-    const numberString = cc.toString()
-    
-    const numberLength = numberString.length
-    
-    const hiddenLength = numberLength-4
-
-    const numberSlice = numberString.slice(hiddenLength, numberLength) 
-    
-    const newNumber = []
-
-    const test = []
-
-    var i = 0
-    while( i < numberLength-4){
-        newNumber.push("#")
-        i += 1
-    }
-
-    newNumber.push(numberSlice)
-
-
-    const numberJoined = newNumber.join('')
-
-    return(
-        numberJoined
-    )
-
-}
-
-const cardNumber =  12156184341153138
+    return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+  }
 
 maskify(cardNumber)
+
+
+
---
+++ running-out-of-space.js
@@ -0,0 +1,10 @@
+function spacey(array){
+
+    newArray = []
+
+    for(var i = 0; i < array.length; i += 1) {
+        
+    }
+}
+
+const testArray = ['this','cheese','has','no','holes']
